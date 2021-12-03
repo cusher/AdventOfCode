bbb35969cf6df912cd56222d89f52df7b2e6441d
cusher <30704914+cusher@users.noreply.github.com>
2021-12-03 03:33:49 -1000

    Clean up code for first 3 days of Advent 2021

--- advent2021.py
+++ advent2021.py
@@ -1,18 +1,15 @@
 def day1():
-    counter = 0
     with open('1.txt') as f:
         nums = [int(val) for val in f.readlines()]
-    for i, val in enumerate(nums):
-        if i != 0 and val > nums[i - 1]:
+    counter = 0
+    for i in range(len(nums) - 1):
+        if nums[i] < nums[i + 1]:
             counter += 1
     print(counter)
     counter = 0
-    for i, val in enumerate(nums):
-        if i != 0 and i + 2 < len(nums):
-            window = nums[i] + nums[i + 1] + nums[i + 2]
-            prev = nums[i-1] + nums[i] + nums[i + 1]
-            if window > prev:
-                counter += 1
+    for i in range(len(nums) - 3):
+        if nums[i] < nums[i + 3]:
+            counter += 1
     print(counter)
 
 
@@ -47,29 +44,22 @@ def day2():
 
 def day3():
     def common_bit(position, values, most):
-        count = 0
-        for v in values:
-            if v[position] == '1':
-                count += 1
+        count = sum(v[position] == '1' for v in values)
         return str(int((count >= len(values) / 2) == most))
 
     with open('3.txt') as f:
         entries = [line.strip() for line in f.readlines()]
-
     n = len(entries[0])
-    gamma = ''.join([common_bit(i, entries, True) for i in range(n)])
-    epsilon = ''.join([common_bit(i, entries, False) for i in range(n)])
-    print(int(gamma, 2) * int(epsilon, 2))
-
-    ratings = [None, None]
+    ratings = [''.join([common_bit(i, entries, bool(option)) for i in range(n)]) for option in range(2)]
+    print(int(ratings[0], 2) * int(ratings[1], 2))
     for option in range(2):
         matches = list(entries)
-        for i in range(len(entries[0])):
-            if ratings[option] is None:
-                cb = common_bit(i, matches, bool(option))
-                matches = list(filter(lambda entry: entry[i] == cb, matches))
-                if len(matches) == 1:
-                    ratings[option] = matches[0]
+        for i in range(n):
+            cb = common_bit(i, matches, bool(option))
+            matches = list(filter(lambda entry: entry[i] == cb, matches))
+            if len(matches) == 1:
+                ratings[option] = matches[0]
+                break
     print(int(ratings[0], 2) * int(ratings[1], 2))
 
 
